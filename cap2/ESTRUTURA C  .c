e8aad1ff11ac2ce26daec506d723667ba3af3d8a
AbrGarcia01 <161896847+AbrGarcia01@users.noreply.github.com>
2025-02-21 18:23:21 -0300

    Add files via upload

---
+++ cap2/ESTRUTURA C  .c
@@ -0,0 +1,26 @@
+// ESTRUTURAS C++
+
+[<declarações de pré-processamento - cabeçalhos>]
+[<declaração do nome de ambiente para definição de seu escopo>]
+[<declaração das variáveis globais>]
+int main([<parâmetros])
+{
+    /*
+    Este trecho é reservado para o corpo da função, com a
+    declaração de suas variáveis locais, seus comandos e funções de trabalho
+    */
+    retuner 0;
+    }
+
+    [
+        [<tipos>] Funçãonome([<parâmetros>])
+        [<declaração de parâmetros>]
+        {
+        /*
+        Este trecho é reservado para o corpo da função, com a
+        declaração de suas variáveis locais, seus comandos e funções de trabalho
+        */
+        [return; ou return(); ou return(valor);]
+        }
+    ]
+}
